84987a9ff4ffa9216236056cdd84101fb4ba1db3
陕杰才 <jiecai.shan@aminer.cn>
2023-11-19 19:08:13 +0800

    debug

--- api/auto_process/eval.py
+++ api/auto_process/eval.py
@@ -27,16 +27,17 @@ if __name__ == '__main__':
     with open("./data/Adarina_test.txt", "r") as f:
         query = f.readlines()
     Adarina_Daniels_res_list = []
-    Adrianna_Corona_res_list = []
-    Anthony_Moore_res_list = []
-    Mallory_Asis_res_list = []
+    # Adrianna_Corona_res_list = []
+    # Anthony_Moore_res_list = []
+    # Mallory_Asis_res_list = []
 
 
     query_list = []
     # 写入excel test4.xlsx
     wb = openpyxl.Workbook()
     ws = wb.active
-    ws.append(["query", "Adarina_Daniels", "Adrianna_Corona", "Anthony_Moore", "Mallory_Asis"])
+    # ws.append(["query", "Adarina_Daniels", "Adrianna_Corona", "Anthony_Moore", "Mallory_Asis"])
+    ws.append(["query", "Adarina_Daniels"])
     # writer = csv.writer(open("./data/test3.csv", "a"))
     # reader = csv.reader(open("./data/test3.csv", "r"))
     # if len(list(reader)) == 0:
@@ -46,17 +47,38 @@ if __name__ == '__main__':
         print(q)
         res = get_result(q, "94aad33c-d817-4da7-917a-fc34df0dedfc")
         Adarina_Daniels_res_list.append(res)
-        res1 = get_result(q, "9fb89632-9904-4471-b2f6-c2cc54a6aa2e")
-        Adrianna_Corona_res_list.append(res1)
-        res2 = get_result(q, "5c321cdb-e7ac-413e-85b6-b631f21fb7a2")
-        Anthony_Moore_res_list.append(res2)
-        res3 = get_result(q, "5e8bcd24-eec2-4df5-bf8e-7b809e7b4265")
-        Mallory_Asis_res_list.append(res3)
+        # res1 = get_result(q, "9fb89632-9904-4471-b2f6-c2cc54a6aa2e")
+        # Adrianna_Corona_res_list.append(res1)
+        # res2 = get_result(q, "5c321cdb-e7ac-413e-85b6-b631f21fb7a2")
+        # Anthony_Moore_res_list.append(res2)
+        # res3 = get_result(q, "5e8bcd24-eec2-4df5-bf8e-7b809e7b4265")
+        # Mallory_Asis_res_list.append(res3)
         query_list.append(q)
-        ws.append([q, res, res1, res2, res3])
+        ws.append([q, res])
         # writer.writerow([q, res, res1, res2, res3])
             # f.write(q + "\t" + res1 + "\t" + res2 + "\t" + res3 + "\n")
-    wb.save("./data/test4.xlsx")
+    wb.save("./data/test_Adarina_Daniels.xlsx")
+
+
+    with open("./data/Maria_test.txt", "r") as f:
+        query = f.readlines()
+    Maria_Paula_Noriega_res_list = []
+
+
+    query_list = []
+    # 写入excel test4.xlsx
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    # ws.append(["query", "Adarina_Daniels", "Adrianna_Corona", "Anthony_Moore", "Mallory_Asis"])
+    ws.append(["query", "Maria_Paula_Noriega"])
+    for q in query:
+        q = ".".join(q.split(".")[1:]).strip()
+        print(q)
+        res = get_result(q, "f1fcf015-8ac2-4634-937f-544211b16c4d")
+        Maria_Paula_Noriega_res_list.append(res)
+        query_list.append(q)
+        ws.append([q, res])
+    wb.save("./data/test_Maria_Paula_Noriega.xlsx")
 
 
     # 整理成csv
