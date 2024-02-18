b85de953d6e510701b0facf4c32598b7080cb54b
陕杰才 <jiecai.shan@aminer.cn>
2024-02-18 20:28:42 +0800

    debug

--- api/controllers/app_api/app/utils.py
+++ api/controllers/app_api/app/utils.py
@@ -7,7 +7,7 @@ import spacy
 
 from mylogger import logger
 
-nlp = spacy.load("en_core_web_md")
+nlp = spacy.load("en_core_web_trf")
 
 app_endpoint = os.getenv("APP_ENDPOINT", "https://www.vvvapp.org")
 
