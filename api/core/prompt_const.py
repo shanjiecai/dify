2ebfa5d8d2e1fd98b8870ea785c5e2fa2570139e
陕杰才 <jiecai.shan@aminer.cn>
2024-04-23 17:27:02 +0800

    debug

--- api/core/prompt_const.py
+++ api/core/prompt_const.py
@@ -3,7 +3,7 @@
 # confident that all the questions have been addressed by the user. Once you reach this point, please append
 # the <finish_question> tag at the end of your current response. Here are the questions: {questions}"""
 
-prompt_question_template = """Sequentially ask the following questions to gather necessary information. If the 
+plan_question_template = """Sequentially ask the following questions to gather necessary information. If the 
 responses to these questions are already present in the previous interactions, do not repeat them. Continue this 
 process until you are confident that all the questions have been addressed by the user. Once you reach this point, 
 please append the "<finish_question>" tag at the end of your current response. Here are the questions: {questions}"""
